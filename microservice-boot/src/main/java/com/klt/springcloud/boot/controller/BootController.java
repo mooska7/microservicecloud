b0eab70a1fb525069fdd234396ec4dcededeb953
zhugq <zhugq@roadefend.com>
2018-12-04 15:18:40 +0800

    first commit

--- microservice-boot/src/main/java/com/klt/springcloud/boot/controller/BootController.java
+++ microservice-boot/src/main/java/com/klt/springcloud/boot/controller/BootController.java
@@ -1,13 +1,16 @@
 package com.klt.springcloud.boot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BootController {
+    @Value("${test.name}")
+    private String name;
 
     @RequestMapping("hello")
     public String hello(){
-        return "hello world";
+        return "hello world" + name;
     }
 }
